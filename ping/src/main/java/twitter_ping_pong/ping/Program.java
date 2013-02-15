1ad078dbb31f6273a9be6f65d1cee00cbab55b68
Vladimir Galkin <vladgalkin@gmail.com>
2013-02-15 23:28:24 +0200

    initial project structure

---
+++ ping/src/main/java/twitter_ping_pong/ping/Program.java
@@ -0,0 +1,7 @@
+package twitter_ping_pong.ping;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ pong/src/main/java/twitter_ping_pong/pong/Program.java
@@ -0,0 +1,7 @@
+package twitter_ping_pong.pong;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
