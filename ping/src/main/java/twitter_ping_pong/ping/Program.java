33defd0b12688ef3a4ba7600365849e653625553
Vladimir Galkin <vladgalkin@gmail.com>
2013-02-15 23:52:50 +0200

    ping execution cycle

--- ping/src/main/java/twitter_ping_pong/ping/Program.java
+++ ping/src/main/java/twitter_ping_pong/ping/Program.java
@@ -1,7 +1,22 @@
 package twitter_ping_pong.ping;
 
-public class Program {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Program {
+    private static final String EXIT_COMMAND = "exit";
+    private static final String PING_COMMAND = "ping";
+
+    public static void main(String[] args) {
+        System.out.printf("Commands:\n");
+        System.out.printf("%s - to ping\n", PING_COMMAND);
+        System.out.printf("%s - to exit\n", EXIT_COMMAND);
+
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        do {
+            System.out.print("Enter command: ");
+            command = scanner.nextLine();
+        } while (!command.equals(EXIT_COMMAND));
     }
 }
